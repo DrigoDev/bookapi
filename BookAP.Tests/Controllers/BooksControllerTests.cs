f030c6bc0862206a5f1da31773e96c1b807f0b2d
Rodrigo Selistre <drigonba@gmail.com>
2025-10-20 00:14:56 -0300

    Added unit tests

---
+++ BookAP.Tests/Controllers/BooksControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using BookAPI.Controllers;
+using BookAPI.Data.Entities;
+using BookAPI.Data.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookAPI.Tests.Controllers
+{
+    public class BooksControllerTests
+    {
+        [Fact]
+        public async void GetAll_ShouldReturnAllBooks()
+        {
+            //Arrange
+            List<Book> books = new List<Book>();
+            books.Add(new Book() { Name = "Book 1" });
+
+            var mock = new Mock<IBookRepository>();
+            mock.Setup(s => s.GetAllAsync()).ReturnsAsync(books);
+
+            var controller = new BooksController(mock.Object);
+
+            //Act
+            var result = controller.GetAll();
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(1, books.Count);
+        }
+    }
+}
---
+++ BookAP.Tests/Repositories/BookRepositoryTests.cs
@@ -0,0 +1,28 @@
+﻿using BookAPI.Data.Entities;
+using BookAPI.Data.Repositories;
+using BookAPI.Data.Repositories.Interfaces;
+using Moq;
+
+namespace BookAPI.Tests.Controllers
+{
+    public class BookRepositoryTests
+    {
+        [Fact]
+        public async void GetAll_ShouldReturnAllBooks()
+        {
+            //Arrange
+            List<Book> books = new List<Book>();
+            books.Add(new Book() { Name = "Book 1" });
+
+            var mock = new Mock<IBookRepository>();
+            mock.Setup(s => s.GetAllAsync()).ReturnsAsync(books);
+
+            //Act
+            var result = await mock.Object.GetAllAsync();
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.Contains(result, b => b.Name == "Book 1");
+        }
+    }
+}
